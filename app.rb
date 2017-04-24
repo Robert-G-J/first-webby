12a9ce9af913e1681c576cca25421b5c87d4de0d
Robert Jones <robert.g.jones@gmail.com>
2017-04-24 16:27:02 +0100

    Installs Sinatra and Shotgun; creates new app file

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+get '/' do
+  "Hello World, you bunch of bastards"
+end
+
+get '/secret' do
+  "How much I loved the group showers and sharing bags"
+end
+
+get '/cornish' do
+  'I really love Cornish Mead'
+end
+
+get '/devon' do
+  'I really love PIPE'
+end
+
+post '/rojo' do
+  'Toga boy'
+end
